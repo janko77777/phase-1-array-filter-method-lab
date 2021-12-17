f006b54f2cdf8b710294c4cbc278cec96353dc17
Steven Jankowski <steven.jankowski1@gmail.com>
2021-12-17 23:19:47 +0000

    commit tree 7b3a858d21acc26564fb92c77af69f3fdf89882b

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution here
+function findMatching(drivers, foo){
+    return drivers.filter(function(str) {
+        return str.toLowerCase().indexOf(foo.toLowerCase()) !== -1
+    })
+}
+  
+function fuzzyMatch(drivers, foo){
+    return drivers.filter(function(string) {
+        let beginningString = string.substring(0,2)
+        return beginningString.toLowerCase().indexOf(foo.toLowerCase()) !== -1
+    })
+}
+
+function matchName(drivers, string){
+    return drivers.filter(function(string) {
+        return drivers.name === string;
+    })
+}
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function matchName(array, string) {
+      return array.filter(array => array.name === string)
+  }
